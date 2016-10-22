9ec60bc9f304bc2f8a653e75f3cef1faf5e276b8
Ravi Sharma <ping2ravi@gmail.com>
2016-10-22 09:05:27 +0100

    Dissected Index.html file

--- src/main/java/com/next/aristotle/controller/view/UserViewController.java
+++ src/main/java/com/next/aristotle/controller/view/UserViewController.java
@@ -23,41 +23,6 @@ public class UserViewController {
         mv.getModel().put("s3_static_content_dir", "https://s3.ap-south-1.amazonaws.com/siorg/website");
         return getModelAndView(mv, request, directory + "/" + fragmentFile);
     }
-//    @RequestMapping(value = {"/me/basic.templates"})
-//    public String basic(ModelAndView mv, HttpServletRequest request) {
-//        return "user/basic";
-//    }
-//
-//    @RequestMapping(value = {"/home", "/index.templates"})
-//    public ModelAndView userHome(ModelAndView mv, HttpServletRequest request) {
-//        mv = getModelAndView(mv, request, "home", "user/home");
-//        return mv;
-//    }
-//
-//    @RequestMapping(value = {"/me/editProfile", "/me/editProfile.templates"})
-//    public ModelAndView userEditProfile(ModelAndView mv, HttpServletRequest request) {
-//        mv = getModelAndView(mv, request, "edit_profile", "user/edit_profile");
-//        return mv;
-//    }
-//
-//    @RequestMapping(value = {"/me/donations", "/me/donations.templates"})
-//    public ModelAndView userDonations(ModelAndView mv, HttpServletRequest request) {
-//        mv = getModelAndView(mv, request, "my_donations", "user/my_donations");
-//        return mv;
-//    }
-//
-//    @RequestMapping(value = {"/me/awaz  ", "/me/awaz.templates"})
-//    public ModelAndView userAwaz(ModelAndView mv, HttpServletRequest request) {
-//        mv = getModelAndView(mv, request, "awaz", "user/awaz");
-//        return mv;
-//    }
-//
-//    @RequestMapping(value = {"/me/{fragmentName}.templates"})
-//    public ModelAndView genericView(ModelAndView mv, HttpServletRequest request, @PathVariable("fragmentName") String fragmentName) {
-//        mv = getModelAndView(mv, request, fragmentName, "user/" + fragmentName);
-//        return mv;
-//    }
-
 
     private ModelAndView getModelAndView(ModelAndView mv, HttpServletRequest request, String fragmentFile) {
         mv.getModel().put("fragmentFile", fragmentFile);
