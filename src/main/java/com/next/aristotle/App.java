6d254b985034e39bd0ed5495409446d1ae16d11b
Kulbhushan sadan <kulbhushan.sadan073@gmail.com>
2016-10-28 13:28:29 +0530

    static and home ,inculdes,pagecontent

--- src/main/java/com/next/aristotle/App.java
+++ src/main/java/com/next/aristotle/App.java
@@ -1,5 +1,4 @@
 package com.next.aristotle;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,30 +6,29 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-
 import java.util.Arrays;
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.next.aristotle"})
 @EnableAutoConfiguration
-public class App extends SpringBootServletInitializer {
-
+public class App extends SpringBootServletInitializer
+{
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
         return application.sources(App.class);
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         ApplicationContext ctx = SpringApplication.run(new Object[]{App.class}, args);
 
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
+        for (String beanName : beanNames)
+        {
             // System.out.println(beanName + " , " + ctx.getBean(beanName));
             System.out.println(beanName);
         }
     }
-
 }
 
